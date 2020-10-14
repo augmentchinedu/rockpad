1a0c56bb0c86b15b195019b1b4796cd9cfdf5a22
augmentchinedu <augmentchinedu@gmail.com>
2020-10-14 11:30:24 +0100

    dev build added

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,10 @@
   <v-app app class="grey lighten-4">
     <nav>
       <v-app-bar app text>
-        <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon
+          class="grey--text"
+          @click="drawer = !drawer"
+        ></v-app-bar-nav-icon>
         <v-toolbar-title class="text-uppercase grey--text">
           <!-- <span class="font-weight-light">Todo</span> -->
           <span class="font-weight-900">Pexmon</span>
@@ -16,89 +19,91 @@
       </v-app-bar>
       <v-navigation-drawer v-model="drawer" app color="#000839">
         <v-layout column align-center>
-          <v-flex class="mt-5"  align-center>
+          <v-flex class="mt-5" align-center>
             <v-col>
-              
-            <v-avatar size="100" ml-3>
-              <img src="./assets/pexmon.png" alt />
-            </v-avatar>
+              <v-avatar size="100" ml-3>
+                <img src="./assets/pexmon.png" alt />
+              </v-avatar>
             </v-col>
-            <p class="white--text text-center subheading mt-1">{{$store.state.hostname}}</p>
+            <p class="white--text text-center subheading mt-1">
+              {{ $store.state.hostname }}
+            </p>
           </v-flex>
           <!-- <v-flex class="mb-3">
           <popup />
           </v-flex>-->
         </v-layout>
 
-        <v-list>
-          <v-list-item v-for="item in items" :key="item.icon" v-bind:to="item.route">
+        <!-- <v-list>
+          <v-list-item
+            v-for="item in items"
+            :key="item.icon"
+            v-bind:to="item.route"
+          >
             <v-list-item-action class="ml-5">
               <v-icon class="white--text">{{ item.icon }}</v-icon>
             </v-list-item-action>
             <v-list-item-content>
               <v-list-item-title class="white--text">
-                {{
-                item.title
-                }}
+                {{ item.title }}
               </v-list-item-title>
             </v-list-item-content>
           </v-list-item>
-        </v-list>
+        </v-list> -->
       </v-navigation-drawer>
     </nav>
-    <v-content class="mx-4 mb-4 mt-4">
-      <router-view></router-view>
-    </v-content>
+    <v-main class="mx-4 mb-4 mt-4">
+      <!-- <router-view></router-view> -->
+    </v-main>
   </v-app>
 </template>
 
 <script>
-export default {
-  name: "App",
-  data() {
-    return {
-      drawer: false,
-      items: [
-        { title: "Home", icon: "aspect_ratio", route: "/home" },
-        
-        { title: "About", icon: "account_circle", route: "/about" },
-        { title: "Expertise", icon: "apartment", route: "/expertise" },
-        { title: "Projects", icon: "folder_open", route: "/projects" },
-        { title: "Team", icon: "people", route: "/team" },
-        { title: "Counter", icon: "people", route: "/counter" },
-        { title: "Contact", icon: "contact_phone", route: "/contact" },
-        { title: "Logo", icon: "branding_watermark", route: "/logo" },
-        { title: "Languages", icon: "g_translate", route: "/languages" },
-        { title: "Settings", icon: "settings", route: "/settings" }
-      ],
-      color: "#fead15",
-      colors: ["blue"],
-      right: false,
-      miniVariant: false,
-      expandOnHover: false,
-      background: true
-    };
-  },
-  computed: {
-    bg() {
-      return this.background
-        ? "https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
-        : undefined;
+  export default {
+    name: "App",
+    data() {
+      return {
+        drawer: false,
+        items: [
+          // { title: "Home", icon: "aspect_ratio", route: "/home" },
+          // { title: "About", icon: "account_circle", route: "/about" },
+          // { title: "Expertise", icon: "apartment", route: "/expertise" },
+          // { title: "Projects", icon: "folder_open", route: "/projects" },
+          // { title: "Team", icon: "people", route: "/team" },
+          // { title: "Counter", icon: "people", route: "/counter" },
+          // { title: "Contact", icon: "contact_phone", route: "/contact" },
+          // { title: "Logo", icon: "branding_watermark", route: "/logo" },
+          // { title: "Languages", icon: "g_translate", route: "/languages" },
+          // { title: "Settings", icon: "settings", route: "/settings" }
+        ],
+        color: "#fead15",
+        colors: ["blue"],
+        right: false,
+        miniVariant: false,
+        expandOnHover: false,
+        background: true
+      };
+    },
+    computed: {
+      bg() {
+        return this.background
+          ? "https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+          : undefined;
+      }
+    },
+    // mounted() {
+    //   this.$store.dispatch("init");
+    // },
+    methods: {
+      signOut() {
+        location.reload();
+      }
     }
-  },
-  // mounted() {
-  //   this.$store.dispatch("init");
-  // },
-  methods: {
-    signOut() {
-      location.reload();
-    }
-  }
-};
+  };
 </script>
 
 <style>
-#app {
-  font-family: "Nunito", sans-serif !important;
-}
+  #app {
+    font-family: "Nunito", sans-serif !important;
+  }
 </style>
